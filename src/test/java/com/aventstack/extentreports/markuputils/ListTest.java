077a8f28d5ac3f56c54b4436a6f99199d5c0c680
anshooarora <anshooarora@gmail.com>
2020-06-14 12:47:19 -0700

    Update ListTest.java

--- src/test/java/com/aventstack/extentreports/markuputils/ListTest.java
+++ src/test/java/com/aventstack/extentreports/markuputils/ListTest.java
@@ -1,14 +1,26 @@
 package com.aventstack.extentreports.markuputils;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ListTest {
+    @Test
+    public void unorderedListFromString() {
+        String items = "Item";
+        Markup m = MarkupHelper.createUnorderedList(items);
+        String s = m.getMarkup();
+        Assert.assertTrue(s.contains("<ul>"));
+        Assert.assertTrue(s.contains("</ul>"));
+        Assert.assertTrue(s.contains("<li>Item</li>"));
+    }
+
     @Test
     public void unorderedListFromArray() {
         String[] items = new String[]{"Item1", "Item2", "Item3"};
@@ -44,7 +56,36 @@ public class ListTest {
         Assert.assertTrue(s.contains("<li>Item2</li>"));
         Assert.assertTrue(s.contains("<li>Item3</li>"));
     }
-    
+
+    @SuppressWarnings("serial")
+    @Test
+    public void unorderedListFromMap() {
+        Map<Object, Object> items = new HashMap<Object, Object>() {
+            {
+                put("Item1", "Value1");
+                put("Item2", "Value2");
+                put("Item3", "Value3");
+            }
+        };
+        Markup m = MarkupHelper.createUnorderedList(items);
+        String s = m.getMarkup();
+        Assert.assertTrue(s.contains("<ul>"));
+        Assert.assertTrue(s.contains("</ul>"));
+        Assert.assertTrue(s.contains("<li>Item1:Value1</li>"));
+        Assert.assertTrue(s.contains("<li>Item2:Value2</li>"));
+        Assert.assertTrue(s.contains("<li>Item3:Value3</li>"));
+    }
+
+    @Test
+    public void orderedListFromString() {
+        String items = "Item1";
+        Markup m = MarkupHelper.createOrderedList(items);
+        String s = m.getMarkup();
+        Assert.assertTrue(s.contains("<ol>"));
+        Assert.assertTrue(s.contains("</ol>"));
+        Assert.assertTrue(s.contains("<li>Item1</li>"));
+    }
+
     @Test
     public void orderedListFromArray() {
         String[] items = new String[]{"Item1", "Item2", "Item3"};
@@ -79,5 +120,24 @@ public class ListTest {
         Assert.assertTrue(s.contains("<li>Item1</li>"));
         Assert.assertTrue(s.contains("<li>Item2</li>"));
         Assert.assertTrue(s.contains("<li>Item3</li>"));
+    }
+
+    @SuppressWarnings("serial")
+    @Test
+    public void orderedListFromMap() {
+        Map<Object, Object> items = new HashMap<Object, Object>() {
+            {
+                put("Item1", "Value1");
+                put("Item2", "Value2");
+                put("Item3", "Value3");
+            }
+        };
+        Markup m = MarkupHelper.createOrderedList(items);
+        String s = m.getMarkup();
+        Assert.assertTrue(s.contains("<ol>"));
+        Assert.assertTrue(s.contains("</ol>"));
+        Assert.assertTrue(s.contains("<li>Item1:Value1</li>"));
+        Assert.assertTrue(s.contains("<li>Item2:Value2</li>"));
+        Assert.assertTrue(s.contains("<li>Item3:Value3</li>"));
     }
 }
