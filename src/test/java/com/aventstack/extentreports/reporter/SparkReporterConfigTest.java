2d0f5280752c08b2cf33cd9779830697fd82cbe0
anshooarora <anshooarora@gmail.com>
2020-07-23 10:38:26 -0700

    Add Spark:config tests

--- src/test/java/com/aventstack/extentreports/reporter/SparkReporterConfigTest.java
+++ src/test/java/com/aventstack/extentreports/reporter/SparkReporterConfigTest.java
@@ -1,5 +1,8 @@
 package com.aventstack.extentreports.reporter;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,5 +20,42 @@ public class SparkReporterConfigTest {
                         .build());
         Assert.assertEquals(spark.getConf().getTheme(), Theme.DARK);
         Assert.assertEquals(spark.getConf().getDocumentTitle(), "MyReport");
+    }
+
+    @Test
+    public void loadJSONConfigFileTest() throws IOException {
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/spark.html");
+        spark.loadJSONConfig(new File("config/spark-config.json"));
+        Assert.assertEquals(spark.getConf().getTheme(), Theme.STANDARD);
+        Assert.assertEquals(spark.getConf().getDocumentTitle(), "ExtentReports");
+        Assert.assertEquals(spark.getConf().getEncoding(), "utf-8");
+    }
+
+    @Test
+    public void loadJSONConfigStringTest() throws IOException {
+        String json = "{ 'theme': 'DARK', 'documentTitle': 'Extent', 'encoding': 'utf-8' }";
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/spark.html");
+        spark.loadJSONConfig(json);
+        Assert.assertEquals(spark.getConf().getTheme(), Theme.DARK);
+        Assert.assertEquals(spark.getConf().getDocumentTitle(), "Extent");
+        Assert.assertEquals(spark.getConf().getEncoding(), "utf-8");
+    }
+
+    @Test
+    public void loadXMLConfigFileTest() throws IOException {
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/spark.html");
+        spark.loadXMLConfig(new File("config/spark-config.xml"));
+        Assert.assertEquals(spark.getConf().getReportName(), "Build 1");
+        Assert.assertEquals(spark.getConf().getDocumentTitle(), "Extent Framework");
+        Assert.assertEquals(spark.getConf().getEncoding(), "UTF-8");
+    }
+
+    @Test
+    public void loadXMLConfigPathTest() throws IOException {
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/spark.html");
+        spark.loadXMLConfig("config/spark-config.xml");
+        Assert.assertEquals(spark.getConf().getReportName(), "Build 1");
+        Assert.assertEquals(spark.getConf().getDocumentTitle(), "Extent Framework");
+        Assert.assertEquals(spark.getConf().getEncoding(), "UTF-8");
     }
 }
