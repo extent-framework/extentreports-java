2c6b2641065f93cdd3b4722e28917c48ca57152e
anshooarora <anshooarora@gmail.com>
2020-09-14 14:50:03 -0700

    update filter

--- src/main/java/com/aventstack/extentreports/reporter/AbstractFilterableReporter.java
+++ src/main/java/com/aventstack/extentreports/reporter/AbstractFilterableReporter.java
@@ -6,6 +6,7 @@ import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.model.Report;
 import com.aventstack.extentreports.model.service.ReportFilterService;
 import com.aventstack.extentreports.reporter.filter.StatusFilterable;
+import com.aventstack.extentreports.util.Assert;
 
 import lombok.Getter;
 
@@ -13,8 +14,7 @@ import lombok.Getter;
 public class AbstractFilterableReporter extends AbstractReporter implements StatusFilterable {
     @Override
     public Report filterAndGet(Report report, Set<Status> set) {
-        if (report == null || report.getTestList().isEmpty())
-            return report;
+        Assert.notNull(report, "Report must not be null");
         if (set != null)
             return ReportFilterService.filter(report, set);
         return report;
--- src/test/java/com/aventstack/extentreports/ParallelTest.java
+++ src/test/java/com/aventstack/extentreports/ParallelTest.java
@@ -1,5 +1,6 @@
 package com.aventstack.extentreports;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.IntStream;
 
 import org.testng.annotations.Test;
@@ -19,11 +20,21 @@ public class ParallelTest {
         extent.attachReporter(new ExtentSparkReporter(""));
         IntStream.range(0, 10000).parallel().forEach(x -> extent.createTest("Test").info(String.valueOf(x)));
     }
-    
+
     @Test
     public void parallelLogs() {
         ExtentReports extent = new ExtentReports();
         ExtentTest test = extent.createTest("Test");
         IntStream.range(0, 10000).parallel().forEach(x -> test.info(String.valueOf(x)));
+    }
+
+    @Test
+    public void parallelFlush() {
+        ExtentReports extent = new ExtentReports();
+        CompletableFuture
+                .runAsync(() -> IntStream.range(0, 10000)
+                        .forEach(x -> extent.createTest("Test" + x).info(String.valueOf(x))));
+        CompletableFuture
+                .runAsync(() -> IntStream.range(0, 10000).forEach(x -> extent.flush()));
     }
 }
