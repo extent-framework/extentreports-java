817ad675baf6e58cc32b918df8a2297c3f8a1807
anshooarora <anshooarora@gmail.com>
2020-12-05 16:20:17 -0800

    fixes #274

--- src/main/java/com/aventstack/extentreports/ExtentTest.java
+++ src/main/java/com/aventstack/extentreports/ExtentTest.java
@@ -305,6 +305,7 @@ public class ExtentTest implements RunResult, Serializable {
     public ExtentTest generateLog(Status status, String details) {
         Log log = Log.builder().status(status).details(details).build();
         model.addGeneratedLog(log);
+        extent.onLogCreated(log, model);
         return this;
     }
 
