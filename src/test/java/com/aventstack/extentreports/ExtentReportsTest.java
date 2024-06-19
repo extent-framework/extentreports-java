bb9937df7fa50ad15bcb7b085d2d044c20c2494e
Anshoo Arora <anshooarora@gmail.com>
2024-06-19 14:27:12 +0530

    Fix GherkinDialect related tests

--- src/test/java/com/aventstack/extentreports/ExtentReportsTest.java
+++ src/test/java/com/aventstack/extentreports/ExtentReportsTest.java
@@ -8,17 +8,22 @@ import org.testng.Assert;
 import com.aventstack.extentreports.gherkin.GherkinDialectManager;
 import com.aventstack.extentreports.gherkin.model.Feature;
 import com.aventstack.extentreports.model.Test;
+import org.testng.annotations.BeforeMethod;
 
 public class ExtentReportsTest {
-    private static final String TEST_NAME = "TEST";
 
-    private ExtentReports extent() {
-        return new ExtentReports();
+    private static final String TEST_NAME = "TEST";
+    private ExtentReports extent;
+
+    @BeforeMethod
+    public void beforeMethod() throws UnsupportedEncodingException {
+        extent = new ExtentReports();
+        extent.setGherkinDialect("en");
     }
 
     @org.testng.annotations.Test
     public void createTestOverloadTypeNameDesc() {
-        ExtentTest test = extent().createTest(Feature.class, TEST_NAME, "Description");
+        ExtentTest test = extent.createTest(Feature.class, TEST_NAME, "Description");
         Test model = test.getModel();
         Assert.assertTrue(model.isBDD());
         Assert.assertEquals(model.getName(), TEST_NAME);
@@ -29,7 +34,7 @@ public class ExtentReportsTest {
 
     @org.testng.annotations.Test
     public void createTestOverloadTypeName() {
-        ExtentTest test = extent().createTest(Feature.class, TEST_NAME);
+        ExtentTest test = extent.createTest(Feature.class, TEST_NAME);
         Test model = test.getModel();
         Assert.assertTrue(model.isBDD());
         Assert.assertEquals(model.getName(), TEST_NAME);
@@ -40,7 +45,7 @@ public class ExtentReportsTest {
 
     @org.testng.annotations.Test
     public void createTestOverloadKeywordNameDesc() throws ClassNotFoundException {
-        ExtentTest test = extent().createTest(new GherkinKeyword("Feature"), TEST_NAME, "Description");
+        ExtentTest test = extent.createTest(new GherkinKeyword("Feature"), TEST_NAME, "Description");
         Test model = test.getModel();
         Assert.assertTrue(model.isBDD());
         Assert.assertEquals(model.getName(), TEST_NAME);
@@ -51,7 +56,7 @@ public class ExtentReportsTest {
 
     @org.testng.annotations.Test
     public void createTestOverloadKeywordName() throws ClassNotFoundException {
-        ExtentTest test = extent().createTest(new GherkinKeyword("Feature"), TEST_NAME);
+        ExtentTest test = extent.createTest(new GherkinKeyword("Feature"), TEST_NAME);
         Test model = test.getModel();
         Assert.assertTrue(model.isBDD());
         Assert.assertEquals(model.getName(), TEST_NAME);
@@ -62,7 +67,7 @@ public class ExtentReportsTest {
 
     @org.testng.annotations.Test
     public void createTestOverloadNameDesc() {
-        ExtentTest test = extent().createTest(TEST_NAME, "Description");
+        ExtentTest test = extent.createTest(TEST_NAME, "Description");
         Test model = test.getModel();
         Assert.assertFalse(model.isBDD());
         Assert.assertEquals(model.getName(), TEST_NAME);
@@ -73,7 +78,7 @@ public class ExtentReportsTest {
 
     @org.testng.annotations.Test
     public void createTestOverloadName() {
-        ExtentTest test = extent().createTest(TEST_NAME);
+        ExtentTest test = extent.createTest(TEST_NAME);
         Test model = test.getModel();
         Assert.assertFalse(model.isBDD());
         Assert.assertEquals(model.getName(), TEST_NAME);
@@ -84,7 +89,6 @@ public class ExtentReportsTest {
 
     @org.testng.annotations.Test
     public void gherkinDialect() throws UnsupportedEncodingException {
-        ExtentReports extent = extent();
         extent.setGherkinDialect("de");
         Assert.assertEquals(GherkinDialectManager.getLanguage(), "de");
     }
@@ -92,7 +96,6 @@ public class ExtentReportsTest {
     @org.testng.annotations.Test
     public void addTestRunnerOutputSingle() {
         String[] logs = new String[]{"Log1", "Log2"};
-        ExtentReports extent = extent();
         Arrays.stream(logs).forEach(extent::addTestRunnerOutput);
         Assert.assertEquals(extent.getReport().getLogs().size(), 2);
         Arrays.stream(logs).forEach(x -> Assert.assertTrue(extent.getReport().getLogs().contains(x)));
@@ -101,7 +104,6 @@ public class ExtentReportsTest {
     @org.testng.annotations.Test
     public void addTestRunnerOutputList() {
         String[] logs = new String[]{"Log1", "Log2"};
-        ExtentReports extent = extent();
         extent.addTestRunnerOutput(Arrays.asList(logs));
         Assert.assertEquals(extent.getReport().getLogs().size(), 2);
         Arrays.stream(logs).forEach(x -> Assert.assertTrue(extent.getReport().getLogs().contains(x)));
